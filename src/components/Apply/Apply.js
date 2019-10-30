dc4d163bd161abee0b416ae42b006298ee8095e4
Shelby Miller <shelbycrosbee@gmail.com>
2019-10-30 16:40:40 -0600

    add edits

--- src/components/Apply/Apply.js
+++ src/components/Apply/Apply.js
@@ -14,11 +14,11 @@ export default class Apply extends Component {
           <Grid item >
           
             <p><b>Background: </b>
-              The Erika Pankow Scholarship Fund was founded in 1997, through generous donations, sponsorships and fundraising events. Members from Big Sky Search and Rescue, Gallatin Canyon Volunteer Fire Department, and Big Sky Ski Patrol originally managed the fund, as Erika was a member of each organization. Today, co-founder Jay Frisque (Mangum) and elected members of the Big Ski Patrol manage the fund. Full or partial scholarships will be given, dependent on the availability of funds in the scholarship account.
+              The Erika Pankow Scholarship Fund was founded in 1997, through generous donations, sponsorships and fundraising events. Members from Big Sky Search and Rescue, Gallatin Canyon Volunteer Fire Department, and Big Sky Ski Patrol originally managed the fund, as Erika was a member of each organization. Today, 3 elected memebers of the Big Sky Ski Patrol manage the scholarships. Full or partial scholarships will be given, dependent on the availability of funds in the scholarship account.
   </p>
             <p>
               <b>Eligibility: </b>
-              There are no requirements and is no preference based on age, gender, race, disability, ethnicity or religion. Preference will be given to individuals who live within the greater Gallatin Valley community, and are able to provide proof of acceptance and ability to successfully complete their desired course. Funds will be made out directly to course financial coordinators or companies. Funds can also be issued retroactively.
+              There are no age requirements, no preference based on age, gender, race, disability, ethnicity or religion. Preference will be given to individuals who live within the greater Gallatin Valley community, and are able to provide proof of acceptance and ability to successfully complete their desired course. Funds will be made out directly to course financial coordinators or companies. Funds can also be issued retroactively.
   </p>
             <p>
               <b>Criteria: </b>
